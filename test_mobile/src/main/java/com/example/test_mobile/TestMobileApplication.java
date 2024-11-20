974003a3f84125effbe06711ab0ce82d66cc1bec
LJH032 <wjdgusdl0989@gmail.com>
2024-11-20 17:07:17 +0900

    모바일 벡엔드서버

---
+++ test_mobile/src/main/java/com/example/test_mobile/TestMobileApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMobileApplication.class, args);
+	}
+
+}
---
+++ test_mobile/src/test/java/com/example/test_mobile/TestMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
